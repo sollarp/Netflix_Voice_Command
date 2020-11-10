87812eb6b52808d734df19264383f781dee0f0c2
sollarp <szpitor@gmail.com>
2020-11-10 19:55:26 +0000

    organized file structure

---
+++ Google_SpeechToText.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Google Cloud Speech API sample application using the streaming API.
+
+NOTE: This module requires the additional dependency `pyaudio`. To install
+using pip:
+
+    pip install pyaudio
+
+Example usage:
+    python transcribe_streaming_mic.py
+"""
+
+# [START speech_transcribe_streaming_mic]
+from __future__ import division
+
+import re
+import sys
+import os
+import tkinter
+import time
+
+from google.cloud import speech_v1 as speech
+from google.cloud.speech_v1 import enums
+from google.cloud.speech_v1 import types
+import pyaudio
+from six.moves import queue
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "YOU JSON API KEY LIKE AI-assist-62996b8e534.json"
+
+# Audio recording parameters
+RATE = 16000
+CHUNK = int(RATE / 10)  # 100ms
+
+
+class MicrophoneStream(object):
+    """Opens a recording stream as a generator yielding the audio chunks."""
+
+    def __init__(self, rate, chunk):
+        self._rate = rate
+        self._chunk = chunk
+
+        # Create a thread-safe buffer of audio data
+        self._buff = queue.Queue()
+        self.closed = True
+
+    def __enter__(self):
+        self._audio_interface = pyaudio.PyAudio()
+        self._audio_stream = self._audio_interface.open(
+            format=pyaudio.paInt16,
+
+            channels=1, rate=self._rate,
+            input=True, frames_per_buffer=self._chunk,
+            # Run the audio stream asynchronously to fill the buffer object.
+            # This is necessary so that the input device's buffer doesn't
+            # overflow while the calling thread makes network requests, etc.
+            stream_callback=self._fill_buffer,
+        )
+
+        self.closed = False
+
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self._audio_stream.stop_stream()
+        self._audio_stream.close()
+        self.closed = True
+        # Signal the generator to terminate so that the client's
+        # streaming_recognize method will not block the process termination.
+        self._buff.put(None)
+        self._audio_interface.terminate()
+
+    def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
+        """Continuously collect data from the audio stream, into the buffer."""
+        self._buff.put(in_data)
+        return None, pyaudio.paContinue
+
+    def generator(self):
+        while not self.closed:
+            # Use a blocking get() to ensure there's at least one chunk of
+            # data, and stop iteration if the chunk is None, indicating the
+            # end of the audio stream.
+            chunk = self._buff.get()
+            if chunk is None:
+                return
+            data = [chunk]
+
+            # Now consume whatever other data's still buffered.
+            while True:
+                try:
+                    chunk = self._buff.get(block=False)
+                    if chunk is None:
+                        return
+                    data.append(chunk)
+                except queue.Empty:
+                    break
+
+            yield b''.join(data)
+
+
+def main():
+    # See http://g.co/cloud/speech/docs/languages
+    # for a list of supported languages.
+    language_code = 'en-GB'  # a BCP-47 language tag
+
+    client = speech.SpeechClient()
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=RATE,
+        language_code=language_code)
+    streaming_config = types.StreamingRecognitionConfig(config=config, interim_results=True)
+
+    with MicrophoneStream(RATE, CHUNK) as stream:
+        audio_generator = stream.generator()
+        requests = (types.StreamingRecognizeRequest(audio_content=content)
+                    for content in audio_generator)
+
+        try:
+            responses = client.streaming_recognize(streaming_config, requests, timeout=21)
+
+        except:
+            no_result = ("no result")
+            return no_result
+        num_chars_printed = 0
+        for response in responses:
+            try:
+                if not response.results:
+                    print("no result ")
+                    continue
+                # The `results` list is consecutive. For streaming, we only care about
+                # the first result being considered, since once it's `is_final`, it
+                # moves on to considering the next utterance.
+                result = response.results[0]
+
+                if not result.alternatives:
+                    print("no alternatives")
+                    continue
+
+                # Display the transcription of the top alternative.
+                transcript = result.alternatives[0].transcript
+
+                # Display interim results, but with a carriage return at the end of the
+                # line, so subsequent lines will overwrite them.
+                #
+                # If the previous result was longer than this one, we need to print
+                # some extra spaces to overwrite the previous result
+                overwrite_chars = ' ' * (num_chars_printed - len(transcript))
+
+                if not result.is_final:
+                    sys.stdout.write(transcript + overwrite_chars + '\r')
+                    sys.stdout.flush()
+                    print("loop no result")
+                    return transcript + overwrite_chars
+
+                num_chars_printed = 0
+            except:
+                no_result = ('')
+                return no_result
+
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,60 @@
+from porcupine import Porcupine
+import struct
+import pyaudio
+from Netflix_control import GoogleSearch
+import pygame
+import os
+
+porcupine = Porcupine(
+    library_path='/home/pi/libpv_porcupine.so',
+    model_file_path='/home/pi/porcupine_params.pv',
+    keyword_file_paths=['/home/pi/bumblebee_raspberry-pi.ppn'],
+    sensitivities=[0.5])
+
+# awake = False
+start_time = None
+wake_time = 3000  # m
+
+# setup audio
+pa = pyaudio.PyAudio()
+audio_stream = pa.open(
+    rate=porcupine.sample_rate,
+    channels=1,
+    format=pyaudio.paInt16,
+    input=True,
+    frames_per_buffer=porcupine.frame_length)
+
+print("I stand ready!")
+s_driver_bool = None
+
+
+class wake_up:
+
+    def wake_active(self):
+        google_search = GoogleSearch()
+        s_driver_bool = None
+        while True:
+            pcm = audio_stream.read(porcupine.frame_length, exception_on_overflow=False)
+            pcm = struct.unpack_from("h" * porcupine.frame_length, pcm)
+            result = porcupine.process(pcm)
+            # result = True
+            if result:
+                # wake-word detected
+                # awake = True
+                print('you sad bumblebee')
+                pygame.mixer.init()
+                pygame.mixer.music.load("bee_sound.wav")
+                pygame.mixer.music.play()
+                os.system('sudo amixer cset numid=1 83%')
+                change = google_search.secondary_voice(s_driver_bool)
+                print(s_driver_bool)
+                print("boolean_back: ")
+                print(change)
+                if change == True:
+                    s_driver_bool = True
+                else:
+                    s_driver_bool = False
+
+
+start = wake_up()
+start.wake_active()
